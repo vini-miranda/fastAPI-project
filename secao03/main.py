905f30a22f477ab2f87a3053c97f2200d2202cc9
Vinicius Miranda <v-alves-miranda@live.com>
2023-06-15 11:24:30 -0300

    metodo put

--- secao03/main.py
+++ secao03/main.py
@@ -42,6 +42,15 @@ async def post_curso(curso: Curso):
     del curso.id
     return curso
 
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
+
 if __name__ == '__main__':
     import uvicorn
 
