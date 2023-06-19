1290a7a217bdd9b2a5acdcf0f5877a9fe7a1f9cc
Vinicius Miranda <v-alves-miranda@live.com>
2023-06-19 18:56:44 -0300

    pydantic

--- secao03/models.py
+++ secao03/models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Curso(BaseModel):
     id: Optional[int] = None
@@ -7,9 +7,17 @@ class Curso(BaseModel):
     aulas: int
     horas: int
 
+    @validator('titulo')
+    def validar_titulo(cls, value):
+        palavras = value.split(' ')
+        if len(palavras) < 3:
+            raise ValueError('O título deve ter pelo menos 3 palavras.')
+        
+        return value
+
 cursos = [
     Curso(id=1, titulo="Programação para Leigos", aulas=112, horas=58),
-    Curso(id=2, titulo="Algoritmos", aulas=30, horas=14),
+    Curso(id=2, titulo="Algoritmos para Leigos", aulas=30, horas=14),
     Curso(id=3, titulo="Lógica de Programação", aulas=42, horas=23)
 ]
 
--- secao03/routes/curso_router.py
+++ secao03/routes/curso_router.py
@@ -28,7 +28,7 @@ async def get_cursos(db: Any = Depends(db_simulator)):
 @api_router.get('/cursos/{curso_id}',
                 description="Retorna um curso da lista", 
                 summary="Retorna um curso",
-                response_model=List[Curso])
+                response_model=Curso)
 async def get_curso(curso_id: int, db: Any = Depends(db_simulator)):
     try:
         curso = cursos[curso_id]
@@ -40,21 +40,19 @@ async def get_curso(curso_id: int, db: Any = Depends(db_simulator)):
           status_code=status.HTTP_201_CREATED,
           description="Insere um novo curso", 
           summary="Posta curso",
-          response_model=List[Curso])
+          response_model=Curso)
 async def post_curso(curso: Curso, db: Any = Depends(db_simulator)):
     next_id: int = len(cursos) + 1
-    cursos[next_id] = curso
-    del curso.id
+    curso.id = next_id
+    cursos.append(curso)
     return curso
 
-@api_router.put('/cursos/{curso_id}',
+@api_router.put('/cursos',
                 description="Edita um curso da lista", 
-                 summary="Edita um curso",
-                response_model=List[Curso])
-async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(db_simulator)):
-    if curso_id in cursos:
-        cursos[curso_id] = curso
-        del curso.id
+                summary="Edita um curso")
+async def put_curso(curso: Curso, db: Any = Depends(db_simulator)):
+    if cursos[curso.id - 1] != None:
+        cursos[curso.id - 1] = curso
         return curso
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
@@ -63,8 +61,8 @@ async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(db_simulator)
                    description="Deleta um curso da lista", 
                    summary="Deleta um curso")
 async def delete_curso(curso_id: int, db: Any = Depends(db_simulator)):
-    if curso_id in cursos:
-        del cursos[curso_id]
+    if cursos[curso_id -1] != None:
+        del cursos[curso_id -1]
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado")
